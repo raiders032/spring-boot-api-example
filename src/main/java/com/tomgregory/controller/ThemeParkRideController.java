32da5daedf4daec2629fc8e279d716899e6ec677
raiders032 <48238562+raiders032@users.noreply.github.com>
2021-12-27 13:17:49 +0900

    Update ThemeParkRideController.java

--- src/main/java/com/tomgregory/controller/ThemeParkRideController.java
+++ src/main/java/com/tomgregory/controller/ThemeParkRideController.java
@@ -19,7 +19,7 @@ public class ThemeParkRideController {
     
     @GetMapping(value = "/hello")
     public String hello() {
-        return "hello3";
+        return "hello4";
     }
 
     @GetMapping(value = "/ride", produces = MediaType.APPLICATION_JSON_VALUE)
